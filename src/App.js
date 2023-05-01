35befeafd0f439ef475f7af9dee2b275a4880174
Hani Darwisha <s190155298@seu.edu.sa>
2023-05-01 09:32:38 +0300

    routing is now working. invigilator home page.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import LoginPage from './LoginPage';
+import InvigilatorHomePage from './invigilator/InvigilatorHomePage';
+import ExamRoomDetailsPage from './invigilator/ExamRoomDetailsPage';
+import StudentDetailsPage from './invigilator/StudentDetailsPage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <LoginPage />
-    </div>
-  );
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route exact path="/" element={<LoginPage />} />
+          <Route exact path="/Invigilator-home" element={<InvigilatorHomePage />} />
+          <Route exact path="/exam-room-details" element={<ExamRoomDetailsPage />} />
+          <Route exact path="/student-details" element={<StudentDetailsPage />} />
+        </Routes>
+      </div>
+    </Router>
+  )
 }
 
 export default App;
+
---
+++ src/invigilator/examRoomData.js
@@ -0,0 +1,54 @@
+const examRoomData = [
+    {
+      id: 1,
+      room: 'Room 107',
+      date: '2023-05-16',
+      time: '10:00 AM',
+    },
+    {
+      id: 2,
+      room: 'Room 105',
+      date: '2023-05-16',
+      time: '02:00 PM',
+    },
+    {
+      id: 3,
+      room: 'Room 116',
+      date: '2022-05-16',
+      time: '10:00 AM',
+    },
+    {
+      id: 4,
+      room: 'Room 108',
+      date: '2022-05-16',
+      time: '02:00 PM',
+    },
+    {
+      id: 1,
+      room: 'Room 107',
+      date: '2023-05-16',
+      time: '10:00 AM',
+    },
+    {
+      id: 2,
+      room: 'Room 105',
+      date: '2023-05-16',
+      time: '02:00 PM',
+    },
+    {
+      id: 3,
+      room: 'Room 116',
+      date: '2022-05-16',
+      time: '10:00 AM',
+    },
+    {
+      id: 4,
+      room: 'Room 108',
+      date: '2022-05-16',
+      time: '02:00 PM',
+    },
+    
+  ];
+  
+  export default examRoomData;
+  
