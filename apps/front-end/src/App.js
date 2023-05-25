b4df366429925c0a4b1e77de23b98167abb012cf
Hani Darwisha <s190155298@seu.edu.sa>
2023-05-25 17:53:54 +0300

    IT-support pages done by 80%, working on it.

--- apps/front-end/src/App.js
+++ apps/front-end/src/App.js
@@ -8,9 +8,11 @@ import StudentDetailsPage from './components/invigilator/studentDetailsPage';
 import QRcodeScannerPage from './components/invigilator/QRcodeScannerPage';
 import AdminHomePage from './components/Admin/AdminHomePage';
 import AdminExamsPage from './components/Admin/AdminExamsPage';
+import AdminTicketsPage from './components/Admin/AdminTicketsPage';
 import Acceptticket from './components/Itsupport/Acceptticket';
 import Closeticket from './components/Itsupport/Closeticket';
-import TicketsPage from './components/Itsupport/TicketsPage';
+import AssignedTicketsPage from './components/Itsupport/AssignedTicketsPage';
+import OpenTicketsITsupportPage from './components/Itsupport/OpenTicketsPage';
 import HeaderAdmin from './components/HeaderAdmin';
 import nonAdminHeader from './components/HeaderNonAdmin';
 import Footer from './components/Footer';
@@ -32,16 +34,19 @@ function App() {
           <Route exact path="/exam-room/:roomID" element={<ExamRoomDetailsPage />} />
           <Route exact path="/Admin-home" element={<AdminHomePage />} />
           <Route exact path="/AdminExamsPage" element={<AdminExamsPage />} />
+          <Route exact path="/Admin/Tickets" element={<AdminTicketsPage />} />
           <Route exact path="/Accept-ticket" element={<Acceptticket />} />
+          <Route exact path="/Accept-ticket/:ticketID" element={<Acceptticket />} />
           {/* <Route exact path="/CloseTicket" element={<Closeticket />} /> */}
           <Route exact path="/CloseTicket/:ticketID" element={<Closeticket />} />
-
-          <Route exact path="/IT-Support/Tickets" element={<TicketsPage />} />
+          <Route exact path="/IT-Support/Open-Tickets" element={<OpenTicketsITsupportPage />} />
+          <Route exact path="/IT-Support/Assigned-Tickets" element={<AssignedTicketsPage />} />
           <Route exact path="/Login" element={<LoginPage />} />
           {/* <Route exact path="/student-details" element={<StudentDetailsPage />} /> */}
-          <Route exact path="/student-details/:seat" element={<StudentDetailsPage />} />
+          <Route exact path="/student-details/:studentSEAT" element={<StudentDetailsPage />} />
           <Route exact path="/QR-code-scanner" element={<QRcodeScannerPage />} />
           <Route exact path="/Request-IT-support" element={<RequestITsupportPage />} />
+
         </Routes>
       </div>
       <Footer />
