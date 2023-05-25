7e7de6fc8436bfb921262db3eebb85b753d80bc5
kalid alyafi <alyafikalid@gmail.com>
2023-05-25 20:38:01 +0300

    Connecting the invigilator page to backend

--- apps/front-end/src/components/invigilator/InvigilatorHomePage.jsx
+++ apps/front-end/src/components/invigilator/InvigilatorHomePage.jsx
@@ -1,22 +1,64 @@
-import React from 'react';
+import { React, useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import examRoomData from './examRoomData'; // import the exam room data
+// import examRoomData from './examRoomData'; // import the exam room data
 import './InvigilatorHomePage.css'; // import the CSS file for this page
 import HeaderNonAdmin from "../HeaderNonAdmin";
 import HeaderAdmin from "../HeaderAdmin";
 
+
 function InvigilatorHomePage() {
-  // map over the exam room data and create a table row for each room
-  const roomRows = examRoomData.map((room) => (
-    <tr key={room.id}>
-      <td><Link to={`/exam-room/${room.id}`}>{room.room}</Link></td>
-      <td>{room.date}</td>
-      <td>{room.time}</td>
-    </tr>
-  ));
 
   // Retrieve user role from state or authentication context, whether it is admin or non-admin, and store it in a variable to be used in the conditional rendering below
   const userRole = 'non-admin';
+
+
+  const [examRoomData, setExamRoomData] = useState([]);
+
+  // This token will be taken from the login. 
+  const token = localStorage.getItem('auth');
+
+  // console.log("THE TOKEN: " + token);
+
+  useEffect(() => {
+    fetch('http://localhost:4000/invigilator/home', {
+      headers: {
+        Authorization: `Bearer ${token}`, // Include the token in the Authorization header
+        'Content-Type': 'application/json'
+      },
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data.message);
+        if (data.message === "Unauthorized") { window.location.href = "../security-stop"; }
+        else { setExamRoomData(data); }
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+
+  }, [token]);
+
+
+  // map over the exam room data and create a table row for each room
+  const roomRows = examRoomData.map((room) => (
+    <tr key={room.id}>
+      <td>
+        <Link to={`/exam-room/${room.id}`}
+          onClick={() => {
+            sessionStorage.setItem('selectedRoom', room.room,);
+            sessionStorage.setItem('selectedTime', room.time);
+          }}
+        >
+          {room.room}
+
+        </Link>
+      </td>
+      <td>{room.date}</td>
+      <td className={`time-${room.id}`}>{room.time}</td>
+    </tr>
+  ));
+
+
 
   return (
     <div>
