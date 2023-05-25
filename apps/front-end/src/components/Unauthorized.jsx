e05d68b0308c136ef3e751bc30dc13acf4908f27
kalid alyafi <alyafikalid@gmail.com>
2023-05-25 20:37:00 +0300

    adding a page to be shown when the token is invalid

---
+++ apps/front-end/src/components/Unauthorized.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Unauthorized.css';
+
+const NotAllowed = () => {
+    return (
+        <div>
+            <h1>Authenticatin error</h1>
+            <h1>we are sorry this page can not be accessed by you, please go back.</h1>
+        </div>
+    );
+};
+
+export default NotAllowed;
