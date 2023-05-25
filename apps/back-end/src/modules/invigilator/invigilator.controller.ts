f1e858624f41785f66e00a3342e72686bbdb7c16
kalid alyafi <alyafikalid@gmail.com>
2023-05-25 23:22:24 +0300

    Fixing repeated codes

--- apps/back-end/src/modules/invigilator/invigilator.controller.ts
+++ apps/back-end/src/modules/invigilator/invigilator.controller.ts
@@ -29,88 +29,85 @@ export class InvigilatorController {
         if (req.user.userType === 'invigilator') {
             return this.invigilatorService.getStudents(getStudnetsDto);
         } else {
-            getStudnets(@Req() req: any, @Query() getStudnetsDto: getStudnetsDto){
-                if (req.user.userType === 'invigilator') {
-                    return this.invigilatorService.getStudents(getStudnetsDto);
-                } else {
-                    throw new UnauthorizedException();
-                }
-            }
-
-            @UseGuards(AuthGuard('jwt'))
-            @Post('Room')
-            markPresnetSwitch(@Req() req: any, @Body() markPresentSwitch: markPresentSwitchDto) {
-                if (req.user.userType === 'invigilator') {
-                    return this.invigilatorService.markPresnetSwitch(markPresentSwitch);
-                } else {
-                    throw new UnauthorizedException();
-                }
-            }
-
-
-
-            @UseGuards(AuthGuard('jwt'))
-            @Post('Room/Scanner')
-            markPresnt(@Req() req: any, @Body('studentID') studentId: string) {
-                if (req.user.userType === 'invigilator') {
-                    const invigilator = req.user.name;
-                    return this.invigilatorService.markPresent(studentId, invigilator);
-
-                } else {
-                    throw new UnauthorizedException();
-                }
-            }
-
-            @UseGuards(AuthGuard('jwt'))
-            @Get('student-details')
-            viewStudentDetails(@Req() req: any, @Query() studentName: string) {
-                if (req.user.userType === 'invigilator') {
-                    return this.invigilatorService.viewStudentDetails(studentName);
-                } else {
-                    throw new UnauthorizedException();
-                }
-            }
-
-            @UseGuards(AuthGuard('jwt'))
-            @Post('student-details')
-            writeReport(@Req() req: any, @Body('studentName') studentInfo: studentInfoDto) {
-                if (req.user.userType === 'invigilator') {
-                    return this.invigilatorService.writeReport(studentInfo);
-                } else {
-                    throw new UnauthorizedException();
-                }
-            }
-
-
-            @UseGuards(AuthGuard('jwt'))
-            @Post('ticket')
-            createTicket(@Req() req: any, @Body() ticketInfo: createTicketDto) {
-                if (req.user.userType === 'invigilator') {
-                    return this.invigilatorService.createTicket(ticketInfo);
-                } else {
-                    throw new UnauthorizedException();
-                }
-            }
-
-            @UseGuards(AuthGuard('jwt'))
-            @Post('generate-password')
-            generatePassword(@Req() req: any, @Body() generatePassword: generatePasswordDto) {
-                if (req.user.userType === 'invigilator') {
-                    return this.invigilatorService.generatePassword(generatePassword);
-                } else {
-                    throw new UnauthorizedException();
-                }
-            }
-
-
-
-
-
-
-
-            @Post()
-            createInvigilators(@Body() createInvigilator: any) {
-                this.invigilatorService.createInvigilators(createInvigilator);
-            }
-
+            throw new UnauthorizedException();
         }
+    }
+
+
+    @UseGuards(AuthGuard('jwt'))
+    @Post('Room')
+    markPresnetSwitch(@Req() req: any, @Body() markPresentSwitch: markPresentSwitchDto) {
+        if (req.user.userType === 'invigilator') {
+            return this.invigilatorService.markPresnetSwitch(markPresentSwitch);
+        } else {
+            throw new UnauthorizedException();
+        }
+    }
+
+
+
+    @UseGuards(AuthGuard('jwt'))
+    @Post('Room/Scanner')
+    markPresnt(@Req() req: any, @Body('studentID') studentId: string) {
+        if (req.user.userType === 'invigilator') {
+            const invigilator = req.user.name;
+            return this.invigilatorService.markPresent(studentId, invigilator);
+
+        } else {
+            throw new UnauthorizedException();
+        }
+    }
+
+    @UseGuards(AuthGuard('jwt'))
+    @Get('student-details')
+    viewStudentDetails(@Req() req: any, @Query() studentName: string) {
+        if (req.user.userType === 'invigilator') {
+            return this.invigilatorService.viewStudentDetails(studentName);
+        } else {
+            throw new UnauthorizedException();
+        }
+    }
+
+    @UseGuards(AuthGuard('jwt'))
+    @Post('student-details')
+    writeReport(@Req() req: any, @Body('studentName') studentInfo: studentInfoDto) {
+        if (req.user.userType === 'invigilator') {
+            return this.invigilatorService.writeReport(studentInfo);
+        } else {
+            throw new UnauthorizedException();
+        }
+    }
+
+
+    @UseGuards(AuthGuard('jwt'))
+    @Post('ticket')
+    createTicket(@Req() req: any, @Body() ticketInfo: createTicketDto) {
+        if (req.user.userType === 'invigilator') {
+            return this.invigilatorService.createTicket(ticketInfo);
+        } else {
+            throw new UnauthorizedException();
+        }
+    }
+
+    @UseGuards(AuthGuard('jwt'))
+    @Post('generate-password')
+    generatePassword(@Req() req: any, @Body() generatePassword: generatePasswordDto) {
+        if (req.user.userType === 'invigilator') {
+            return this.invigilatorService.generatePassword(generatePassword);
+        } else {
+            throw new UnauthorizedException();
+        }
+    }
+
+
+
+
+
+
+
+    @Post()
+    createInvigilators(@Body() createInvigilator: any) {
+        this.invigilatorService.createInvigilators(createInvigilator);
+    }
+
+}
