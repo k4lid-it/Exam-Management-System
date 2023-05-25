7978a4f7eaa9d6bbd3bb80192bebbfa54a9ad3c9
kalid alyafi <alyafikalid@gmail.com>
2023-05-25 20:38:31 +0300

    adding logic to the login page

--- apps/front-end/src/components/Login/LoginPage.jsx
+++ apps/front-end/src/components/Login/LoginPage.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import "./LoginPage.css";
+import axios from 'axios';
+
 
 function Login() {
   const [username, setUsername] = useState("");
@@ -20,13 +22,55 @@ function Login() {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    // handle login logic
-  };
+
+
+    // fetch('http://localhost:4000/auth/login', {
+    //   method: 'POST',
+    //   // headers: {
+    //   //   // Authorization: `Bearer ${token}`, // Include the token in the Authorization header
+    //   //   'Content-Type': 'application/json'
+    //   // },
+
+    //   body: {
+    //     "email": "h.alzahrani@seu.edu.sa",
+    //     "password": "dan-2023"
+    //   }
+    // })
+    //   .then(response => response.text())
+    //   .then(data => {
+    //     console.log("Token: " + data)
+    //     document.cookie = `authcookie=${data}; expires=130; path=/`;
+    //   })
+    //   .catch(error => {
+    //     console.error('Error:', error);
+    //   });
+
+    axios.post('http://localhost:4000/auth/login', {
+      email: username,
+      password: password
+    })
+      .then(response => {
+
+        //// Save the token in a cookie
+        //// document.cookie = `authcookie=${response.data.access_token
+        ////   }; expires=130; path=/`;
+
+        //Save the token in a local storage
+        localStorage.setItem('auth', response.data.access_token);
+        document.location.href = "Invigilator-home";
+      })
+      .catch(error => {
+        // Log the error
+        console.error(error);
+      });
+
+  }
+    ;
 
   return (
     <div className="login-page">
       <div className="login-form">
-      <img src="seuLogo.png" alt="Logo" className="logo" />
+        <img src="seuLogo.png" alt="Logo" className="logo" />
         <h1 className="title">Login using SEU account</h1>
         <form onSubmit={handleSubmit}>
           <div className="form-group">
