82d8d08f91449732a341ad2361be2f2e7320430d
Hani Darwisha <s190155298@seu.edu.sa>
2023-05-26 23:45:22 +0300

    mock-exam pages added+QRcode generator inside them

---
+++ apps/front-end/src/MockExam/studentExamPage1.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react';
+import QRCode from 'qrcode.react';
+import './mockExam.css';
+
+export default function QRCodeGeneratorStudent1() {
+  const [studentData, setStudentData] = useState({ id: '', name: '' });
+  const [qrCodeValue, setQRCodeValue] = useState('');
+
+  useEffect(() => {
+    // Simulating the fetching of student data from the backend
+    fetchStudentData().then((data) => {
+      setStudentData(data);
+      generateQRCode(data);
+    });
+  }, []);
+
+  const fetchStudentData = async () => {
+    // Simulating the API call to fetch student data
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve({ id: 's190155298', name: 'Hani Ghassan Darwicheh' });
+      }, 1000);
+    });
+  };
+
+  const generateQRCode = (data) => {
+    const qrCodeValue = `${data.id},${data.name}`;
+    setQRCodeValue(qrCodeValue);
+  };
+
+  return (
+    <div>
+      <h2>Please ask your Invigilator to scan your QR code to gain exam access:</h2>
+      <h1>Student: {studentData.name}</h1>
+      <QRCode 
+      value={qrCodeValue}
+    //   value='seu.edu.sa'
+      size={390}
+      />
+    </div>
+  );
+}
