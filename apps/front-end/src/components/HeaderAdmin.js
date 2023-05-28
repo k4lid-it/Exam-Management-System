4b5c7ecf2729b0e9fc4dfe099ad648ba92f819b7
kalid alyafi <alyafikalid@gmail.com>
2023-05-28 08:55:45 +0300

    LogOut feature added

--- apps/front-end/src/components/HeaderAdmin.js
+++ apps/front-end/src/components/HeaderAdmin.js
@@ -15,25 +15,26 @@ const Nav = () => {
     <nav>
       {/* <div className="logo">Exam Invigilation Portal</div> */}
       <ul className="header-items">
-    <Link to="/Login">
-        <li className="logo-item"> 
-          <img src="https://icon-library.com/images/blue-exit-icon/blue-exit-icon-8.jpg" alt="Logout Logo" />
-        </li>
-    </Link>
-    
-    <Link to="/Admin-home">
-        <li className="logo-item"> 
-          <img src={process.env.PUBLIC_URL + '/home-button-icon.png'} alt="Home Logo" />
-        </li>
-    </Link>
+        <Link to="/Login"
+          onClick={() => { localStorage.setItem('auth', ' ') }}>
+          <li className="logo-item">
+            <img src="https://icon-library.com/images/blue-exit-icon/blue-exit-icon-8.jpg" alt="Logout Logo" />
+          </li>
+        </Link>
+
+        <Link to="/Admin-home">
+          <li className="logo-item">
+            <img src={process.env.PUBLIC_URL + '/home-button-icon.png'} alt="Home Logo" />
+          </li>
+        </Link>
         <li className="dropdown" onClick={toggleDropdown}>
-        <img className='logo-item' src={process.env.PUBLIC_URL + '/dropdown-button-icon.png'} alt="dropdown icon" />
+          <img className='logo-item' src={process.env.PUBLIC_URL + '/dropdown-button-icon.png'} alt="dropdown icon" />
           {isOpen && (
             <ul className="dropdown-menu">
-          <Link to="/AdminExamsPage">    <li>Exam rooms</li>     </Link>
-          <Link to="/Admin/assigned-Tickets">    <li>My tickets</li>     </Link>
-          <Link to="/Admin/Tickets">    <li>All tickets</li>     </Link>
-          <Link to="/Admin/Tickets">    <li>QR Code scanner</li>     </Link>
+              <Link to="/AdminExamsPage">    <li>Exam rooms</li>     </Link>
+              <Link to="/Admin/assigned-Tickets">    <li>My tickets</li>     </Link>
+              <Link to="/Admin/Tickets">    <li>All tickets</li>     </Link>
+              <Link to="/Admin/Tickets">    <li>QR Code scanner</li>     </Link>
 
 
             </ul>
