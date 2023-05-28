d260ac3f73ffa88105ed5cf84049cadde4ed5796
kalid alyafi <alyafikalid@gmail.com>
2023-05-28 06:47:14 +0300

    working assigned page

--- apps/front-end/src/components/Itsupport/AssignedTicketsPage.jsx
+++ apps/front-end/src/components/Itsupport/AssignedTicketsPage.jsx
@@ -3,41 +3,14 @@ import { Link } from 'react-router-dom';
 import './ITtickets.css';
 import HeaderIT from '../HeaderIT';
 
-
-
 function ITsupportPage() {
-  // Mock data for IT support tickets
-  const ITSupportTicketData = [
-    {
-      id: 1,
-      room: 'Room A',
-      date: '2023-05-21',
-      time: '10:00 AM',
-      examPeriod: 'Morning',
-      service: 'Service 1',
-      state: 'In progress'
-    },
-    {
-      id: 2,
-      room: 'Room B',
-      date: '2023-05-21',
-      time: '2:00 PM',
-      examPeriod: 'Afternoon',
-      service: 'Service 2',
-      state: 'Closed',
-    },
-    // Add more IT support ticket data here...
-  ];
-
-  let [examRoomData, setExamRoomData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [examRoomData, setExamRoomData] = useState([]);
   const token = localStorage.getItem('auth');
-
-  examRoomData = [{
-
-  }]
-
   const url = `http://localhost:4000/support/My-ticket`;
+
   useEffect(() => {
+    setIsLoading(true);
     fetch(url, {
       headers: {
         Authorization: `Bearer ${token}`,
@@ -46,28 +19,22 @@ function ITsupportPage() {
     })
       .then(response => response.json())
       .then(data => {
-        if (data.message === "Unauthorized") { window.location.href = "../security-stop"; }
-        else {
+        if (data.message === "Unauthorized") {
+          window.location.href = "../security-stop";
+        } else {
           setExamRoomData(data);
-          console.log(examRoomData);
         }
+        setIsLoading(false);
       })
       .catch(error => {
         console.error('Error:', error);
+        setIsLoading(false);
       });
-
   }, [token]);
 
-  console.log(examRoomData);
-
   // Map over the IT support ticket data and create a table row for each ticket
-  const ticketRows = examRoomData.map((ticket) => ( //will be examRoomData.map
+  const ticketRows = examRoomData.map((ticket) => (
     <tr key={ticket.id}>
-
-      {/* <td>
-        <Link to={`/CloseTicket/${ticket.id}`}>View Ticket</Link>
-      </td> */}
-
       <td>
         {ticket.status === 'In progress' ? (
           <Link to={`/closeTicket/${ticket.id}`}>View Ticket</Link>
@@ -75,11 +42,8 @@ function ITsupportPage() {
           <span></span>
         )}
       </td>
-
       <td>{ticket.room}</td>
-      {/* <td>{ticket.date}</td> */}
       <td>{ticket.time}</td>
-      {/* <td>{ticket.examPeriod}</td> */}
       <td>{ticket.type}</td>
       <td>{ticket.status}</td>
     </tr>
@@ -88,28 +52,28 @@ function ITsupportPage() {
   return (
     <div>
       <HeaderIT />
-
       <div className='ITsupportPage-container'>
-
-        <Link to="../IT-support/open-tickets"><button className='btn'>view Open Tickets</button></Link>
-
+        <Link to="../IT-support/open-tickets">
+          <button className='btn'>View Open Tickets</button>
+        </Link>
         <h1>IT support tickets assigned to me:</h1>
-        <table>
-          <thead>
-            <tr>
-              <th></th>
-              <th>Room</th>
-              {/* <th>Date</th> */}
-              <th>Time</th>
-              {/* <th>Exam Period</th> */}
-              <th>Service</th>
-              <th>State</th>
-            </tr>
-          </thead>
-          <tbody>{ticketRows}</tbody>
-        </table>
+        {isLoading ? (
+          <p>Loading...</p>
+        ) : (
+          <table>
+            <thead>
+              <tr>
+                <th></th>
+                <th>Room</th>
+                <th>Time</th>
+                <th>Service</th>
+                <th>Status</th>
+              </tr>
+            </thead>
+            <tbody>{ticketRows}</tbody>
+          </table>
+        )}
       </div>
-      {/* <Link to="/IT-support/open-tickets"><button className='btn'>view Open Tickets</button></Link> */}
     </div>
   );
 }
