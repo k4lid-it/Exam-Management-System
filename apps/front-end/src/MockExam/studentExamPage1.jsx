21576b9cdd042b3eebefa390866ea76ef7244f28
Hani Darwisha <s190155298@seu.edu.sa>
2023-05-30 19:47:31 +0300

    -fixed responsive issue -added login page link in logut successful page -fixed the submit button to the heroku url -fixed mock exam qr code background issue

--- apps/front-end/src/MockExam/studentExamPage1.jsx
+++ apps/front-end/src/MockExam/studentExamPage1.jsx
@@ -44,12 +44,13 @@ export default function QRCodeGeneratorStudent1() {
     <div>
       <h2>Please ask your Invigilator to scan your QR code to gain exam access:</h2>
       <h1>Student: {studentData.name}</h1>
+      <div className='qr-container'>
       <QRCode
         value={qrCodeValue}
         //   value='seu.edu.sa'
         size={390}
       />
-
+      </div>
       <div className="input-button">
         <h2>or Ask your invigilator for your exam password:</h2>
         <input type="text" value={textValue} placeholder="Enter Exam Password" />
